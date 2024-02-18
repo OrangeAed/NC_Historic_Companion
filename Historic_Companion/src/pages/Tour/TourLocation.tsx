a6045a40ed8062052e11a5b50b12e641beabcae3
OrangeAed <ajsimonsrudolph@gmail.com>
2024-02-18 14:02:22 -0500

    Added Functionality to switch between show more and show less

--- Historic_Companion/src/pages/Tour/TourLocation.tsx
+++ Historic_Companion/src/pages/Tour/TourLocation.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
+// TourLocation.tsx
+import React, { useState } from 'react';
 import ReactAudioPlayer from 'react-audio-player';
 
-
 function TourLocation(props: any) {
+    const [showFullText, setShowFullText] = useState(false);
 
     return (
         <div className="tour-location">
             <h2>{props.name}</h2>
             <img src={props.image} alt="Tour Location"/>
             <ReactAudioPlayer src={props.audio} controls />
-            <p>{props.abridgedText}</p>
-            <button>More Details</button>
+            <p>{showFullText ? props.text : props.abridgedText}</p>
+            <button onClick={() => setShowFullText(!showFullText)}>
+                {showFullText ? 'Show Less' : 'Show More'}
+            </button>
         </div>
     );
 }
+
 export default TourLocation;
