73315edb85edbee5b35ad452a0cab0c0a4b0117c
OrangeAed <ajsimonsrudolph@gmail.com>
2024-03-19 16:25:05 -0400

    This is not working, but I am using this commit as a historical reference because I am deleting the current server/api code

--- nc-historic-companion/server/server.js
+++ nc-historic-companion/server/server.js
@@ -1,49 +1,54 @@
-import * as express from 'express';
-import { Request, Response } from 'express';
-import * as fs from 'fs';
-import * as path from 'path';
-import * as bodyParser from 'body-parser';
-import * as cors from 'cors';
-
-const app = express();
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var fs = require("fs");
+var path = require("path");
+var bodyParser = require("body-parser");
+var cors = require("cors");
+var url_1 = require("url");
+var path_1 = require("path");
+var __filename = (0, url_1.fileURLToPath)(import.meta.url);
+var __dirname = (0, path_1.dirname)(__filename);
+var app = express();
 app.use(bodyParser.json());
 app.use(cors());
-
 // Define the path to the tours.json file
-const toursPath = path.join(__dirname, 'tours.json');
-
+var toursPath = path.join(__dirname, 'tours.json');
 // Handle GET requests for the tours
-app.get('/api/tours', (req: Request, res: Response) => {
-    fs.readFile(toursPath, 'utf8', (err, data) => {
+app.get('/api/tours', function (req, res) {
+    fs.readFile(toursPath, 'utf8', function (err, data) {
         if (err) {
             console.error('Error reading tours.json:', err);
             res.status(500).json({ error: 'Error reading tours.json' });
-        } else {
+        }
+        else {
             console.log('Data from tours.json:', data);
             res.json(JSON.parse(data));
         }
     });
 });
-
-app.post('/api/tours', (req: Request, res: Response) => {
+app.post('/api/tours', function (req, res) {
     console.log('Received data:', req.body);
-    fs.readFile(toursPath, 'utf8', (err, data) => {
+    fs.readFile(toursPath, 'utf8', function (err, data) {
         if (err) {
             console.error('Error reading tours.json:', err);
             res.status(500).send('Error reading tours.json');
-        } else {
-            const tours = JSON.parse(data);
-            const sanitizedTitle = req.body.title.replace(/[^a-z0-9]/gi, '_').toLowerCase();
+        }
+        else {
+            var tours = JSON.parse(data);
+            var sanitizedTitle = req.body.title.replace(/[^a-z0-9]/gi, '_').toLowerCase();
             if (tours[sanitizedTitle]) {
                 console.error('A tour with this title already exists. Please choose a different title.');
                 res.status(400).send('A tour with this title already exists. Please choose a different title.');
-            } else {
+            }
+            else {
                 tours[sanitizedTitle] = req.body;
-                fs.writeFile(toursPath, JSON.stringify(tours), 'utf8', (err) => {
+                fs.writeFile(toursPath, JSON.stringify(tours), 'utf8', function (err) {
                     if (err) {
                         console.error('Error writing to tours.json:', err);
                         res.status(500).send('Error writing to tours.json');
-                    } else {
+                    }
+                    else {
                         console.log('Tour created successfully');
                         res.send('Tour created successfully');
                     }
@@ -52,4 +57,4 @@ app.post('/api/tours', (req: Request, res: Response) => {
         }
     });
 });
-app.listen(3000, () => console.log('Server listening on port 3000'));
+app.listen(3000, function () { return console.log('Server listening on port 3000'); });
